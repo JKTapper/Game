5c113961df267cf5bdb07185f33cf43a7f316c67
JKTapper <108480207+JKTapper@users.noreply.github.com>
2024-09-08 13:05:45 +0100

    Update Game.py
    The code has been further simplified to bring more relevant sections into the Game class and the system for resetting the game has  been turned into a larger loop.

--- Game.py
+++ Game.py
@@ -4,9 +4,22 @@ import random
 
 class Game:
     
-    def __init__(self,cells,empty_cells):
+    def __init__(self,width,height):
+        cells = {}
+        empty_cells = set({})
+        for x in range(width):
+            for y in range(height):
+                cells[(x,y)] = '.'
+                empty_cells.add((x,y))
         self.cells = cells
-        self.available_cells = empty_cells
+        self.empty_cells = empty_cells
+        horizontal = [[(x,y) for x in range(0,width)] for y in range(0,height)]
+        vertical = [[(x,y) for y in range(0,height)] for x in range(0,width)]
+        lines = horizontal + vertical
+        if width == height:
+            diagonal = [[(x,x) for x in range(width)],[(x,width - x - 1) for x in range(width)]]
+            lines += diagonal
+        self.winning_lines = lines
 
     def __str__(self):
         rows = ['|'.join(['',*[self.cells[(x,y)] for x in range(0,width)],''])for y in range(height)]
@@ -14,7 +27,7 @@ class Game:
 
     def do_move(self,move,player):
         self.cells[move] = player
-        self.available_cells.remove(move)
+        self.empty_cells.remove(move)
 
     def check_if_board_square_is_empty(self,move):
         return move in self.empty_cells
@@ -25,98 +38,58 @@ class Game:
             player_action = tuple([int(coord) for coord in coords])
             return(player_action)
 
-    def generate_computer_move():
-        move = random.choice(list(empty_cells))
+    def generate_computer_move(self):
+        move = random.choice(list(self.empty_cells))
         return move
 
-    def check_if_move_is_available(move):
-        if move not in cells.keys():
+    def check_if_move_is_available(self,move):
+        if move not in self.cells.keys():
             print('That is outside of the game board.')
             return False
-        if move not in empty_cells:
+        if move not in self.empty_cells:
             print('That space is taken.')
             return False
         return True
 
-    def check_if_player_has_won(player):
-        horizontal = [[(x,y) for x in range(0,width)] for y in range(0,height)]
-        veritical = [[(x,y) for y in range(0,height)] for x in range(0,width)]
-        lines = horizontal + veritical
-        if width == height:
-            diagonal = [[(x,x) for x in range(width)],[(x,width - x - 1) for x in range(width)]]
-            lines += diagonal
-        player_won = any([all([cells[cell] == player for cell in line]) for line in lines])
+    def check_if_player_has_won(self,player):
+        player_won = any([all([self.cells[cell] == player for cell in line]) for line in self.winning_lines])
         return player_won
         
-
-def player_prompt():
-    return 'Please enter the coordinates of where you want to play X,Y, where X=0-n, Y=0-n'
-
-def reset():
-    global game
-    global turn
-    global human
-    game = copy.deepcopy(begin)
-    turn = 0
-    human = input('Do you want to play first(X) or second(O)')
-
-def game_over():
-        input('Play again?')
-        reset()
-
-def end_game_value():
-    if check_if_player_has_won(player):
-        return 1
-    board_full = len(game.available_cells) == 0
-    if board_full:
-        return 0
-    return -1
-
-def evaluate(move):
-    pass
+    def get_player_prompt(self):
+        return 'Please enter the coordinates of where you want to play X,Y, where X=0-n, Y=0-n'
 
 width = 4
 height = 4
-one_row = ['.']*width
-begining_game_state = {}
-all_cells = set({})
-for x in range(width):
-    for y in range(height):
-        begining_game_state[(x,y)] = '.'
-        all_cells.add((x,y))
-begin = Game(begining_game_state,all_cells)
-game = copy.deepcopy(begin)
+begin = Game(width,height)
 players = {0:'X',1:'O'}
 player = 'X'
-turn = 0
-human = input('Do you want to play first(X) or second(O)')
-    
 while True:
-    print(game)
-    end_game = end_game_value()
-    if end_game == 1:
-        print(f'{player} won!')
-        game_over()
-        continue
-    elif end_game == 0:
-        print('It\'s a draw.')
-        game_over()
-        continue
-    player = players[turn % 2]
-    if player == human:
-        player_input = input(player_prompt())
-        if player_input == 'reset':
-            reset()
-            continue
-        try:
-            move = validate_player_input_format(game,player_input.strip())
-        except:
-            print('Please enter a valid input')
-            continue
-    else:
-        move = computer_move()
-        print(f'The computer plays in position {move}.')
-    if not check_if_move_is_available(move):
-            continue
-    game.do_move(move,player)
-    turn +=1
+    game = copy.deepcopy(begin)
+    turn = 0
+    human = input('Do you want to play first(X) or second(O)')
+    while True:
+        print(game)
+        if game.check_if_player_has_won(player):
+            print(f'{player} won!')
+            break   
+        elif game.empty_cells == {}:
+            print('It\'s a draw.')
+            break
+        player = players[turn % 2]
+        if player == human:
+            player_input = input(game.get_player_prompt())
+            if player_input == 'reset':
+                break
+            try:
+                move = game.validate_player_input_format(player_input.strip())
+            except:
+                print('Please enter a valid input')
+                continue
+        else:
+            move = game.generate_computer_move()
+            print(f'The computer plays in position {move}.')
+        if not game.check_if_move_is_available(move):
+                continue
+        game.do_move(move,player)
+        turn +=1
+    input('Play again?')
